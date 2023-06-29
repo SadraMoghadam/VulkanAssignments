70179cde445b3ab2563bd7fd603b93f2de7072de
SadraHeidariMoghadam <sadra_h_m@outlook.com>
2023-06-29 22:59:39 +0200

    Completed code for creating triangle and rectangle and created a cube

--- A14_SLN/A14/A14/primGen.hpp
+++ A14_SLN/A14/A14/primGen.hpp
@@ -1,3 +1,26 @@
+void create_triangle(std::vector<Vertex>& vDef, std::vector<uint32_t>& vIdx, glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 norm)
+{
+	int startingPoint = vDef.size();
+	vDef.push_back({ pos1, norm });
+	vDef.push_back({ pos2, norm });
+	vDef.push_back({ pos3, norm });
+
+	vIdx.push_back(startingPoint); vIdx.push_back(startingPoint + 1); vIdx.push_back(startingPoint + 2);
+}
+
+void create_rectangle(std::vector<Vertex>& vDef, std::vector<uint32_t>& vIdx, glm::vec3 pos1, glm::vec3 pos2, glm::vec3 pos3, glm::vec3 pos4, glm::vec3 norm)
+{
+
+	int startingPoint = vDef.size();
+	vDef.push_back({ pos1, norm });
+	vDef.push_back({ pos2, norm });
+	vDef.push_back({ pos3, norm });
+	vDef.push_back({ pos4, norm });
+
+	vIdx.push_back(startingPoint); vIdx.push_back(startingPoint + 1); vIdx.push_back(startingPoint + 2);
+	vIdx.push_back(startingPoint + 1); vIdx.push_back(startingPoint + 2); vIdx.push_back(startingPoint + 3);
+}
+
 void Assignment14::createCubeMesh(std::vector<Vertex> &vDef, std::vector<uint32_t> &vIdx) {
 	// The procedure fills array vPos with the positions of the vertices and of the normal vectors of the mesh
 /*	vDef.push_back({{0.0f, 0.0f, 0.1f}, {0.5f, 0.0f, 0.866f}});	// vertex 0 - Position and Normal
@@ -12,13 +35,39 @@ void Assignment14::createCubeMesh(std::vector<Vertex> &vDef, std::vector<uint32_
 	// The primitive built here is a cube centered in the origin, whose edge lenght is 2.
 */	
 	// Fill array vPos with the positions of the vertices of the mesh
-	vDef.push_back({{0.0f, 0.0f, 0.1f}, {0.5f, 0.0f, 0.866f}});	// vertex 0 - Position and Normal
-	vDef.push_back({{1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f}});	// vertex 1 - Position and Normal
-	vDef.push_back({{0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f}});	// vertex 2 - Position and Normal
-	vDef.push_back({{1.0f, 1.0f, 0.1f}, {-0.5f, 0.0f, 0.866f}});// vertex 3 - Position and Normal
-	// Fill the array vIdx with the indices of the vertices of the triangles
-	vIdx.push_back(0); vIdx.push_back(1); vIdx.push_back(2);	// First triangle
-	vIdx.push_back(1); vIdx.push_back(2); vIdx.push_back(3);	// Second triangle
+	/*vDef.push_back({ {0.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 0.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 0.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 0.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {0.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 0.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 0.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 0.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 1.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 1.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 0.0f, 1.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {0.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 1.0f}, {1.0f, 0.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 1.0f}, {0.0f, 1.0f, 0.0f} });
+	vDef.push_back({ {1.0f, 1.0f, 1.0f}, {0.0f, 0.0f, 1.0f} });
+	
+	vIdx.push_back(0); vIdx.push_back(1); vIdx.push_back(2);	
+	vIdx.push_back(1); vIdx.push_back(2); vIdx.push_back(3);*/
+	create_rectangle(vDef, vIdx, { 0, 0, 0 }, { 0, 0, 1 }, { 0, 1, 0 }, { 0, 1, 1 }, { 1, 0, 0 });
+	create_rectangle(vDef, vIdx, { 1, 0, 0 }, { 1, 0, 1 }, { 1, 1, 0 }, { 1, 1, 1 }, { 1, 0, 0 });
+	create_rectangle(vDef, vIdx, { 0, 0, 0 }, { 0, 0, 1 }, { 1, 0, 0 }, { 1, 0, 1 }, { 0, 1, 0 });
+	create_rectangle(vDef, vIdx, { 0, 1, 0 }, { 0, 1, 1 }, { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 0 });
+	create_rectangle(vDef, vIdx, { 0, 0, 0 }, { 0, 1, 0 }, { 1, 0, 0 }, { 1, 1, 0 }, { 0, 0, 1 });
+	create_rectangle(vDef, vIdx, { 0, 0, 1 }, { 0, 1, 1 }, { 1, 0, 1 }, { 1, 1, 1 }, { 0, 0, 1 });
 }
 
 void Assignment14::createFunctionMesh(std::vector<Vertex> &vDef, std::vector<uint32_t> &vIdx) {
